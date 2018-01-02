c938f2121fe75bf8184341483ecd9c7a81bb7e3d
Joachim Metz <joachim.metz@gmail.com>
2018-01-02 20:04:33 +0100

    Applied updates

--- libcfile/libcfile.c
+++ libcfile/libcfile.c
@@ -1,7 +1,7 @@
 /*
  * Library to support cross-platform C file functions
  *
- * Copyright (C) 2008-2017, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2018, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
