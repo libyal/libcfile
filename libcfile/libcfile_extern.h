21a269a71d6c69d78b8401045d9dbf490fe0efbc
Joachim Metz <joachim.metz@gmail.com>
2020-12-08 08:10:18 +0100

    Applied updates

--- libcfile/libcfile_extern.h
+++ libcfile/libcfile_extern.h
@@ -30,7 +30,7 @@
 
 #include <libcfile/extern.h>
 
-#if defined( __CYGWIN__ )
+#if defined( __CYGWIN__ ) || defined( __MINGW32__ )
 #define LIBCFILE_EXTERN_VARIABLE	extern
 #else
 #define LIBCFILE_EXTERN_VARIABLE	LIBCFILE_EXTERN
