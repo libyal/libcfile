444eae07650dc07b4c7a0e89a4280e0660b97179
Joachim Metz <joachim.metz@gmail.com>
2026-06-04 09:19:07 +0200

    Worked on tests and changes for CI tests

--- libcfile/libcfile_extern.h
+++ libcfile/libcfile_extern.h
@@ -24,6 +24,19 @@
 
 #include <common.h>
 
+#if !defined( __CYGWIN__ ) && !defined( _WIN32 ) && defined( __has_attribute )
+#if __has_attribute( visibility )
+#define LIBCFILE_INTERNAL	__attribute__((visibility("hidden"))) extern
+
+#else
+#define LIBCFILE_INTERNAL	extern
+
+#endif /* __has_attribute( visibility ) */
+#else
+#define LIBCFILE_INTERNAL	extern
+
+#endif /* !defined( __CYGWIN__ ) && !defined( _WIN32 ) && defined( __has_attribute ) */
+
 /* Define HAVE_LOCAL_LIBCFILE for local use of libcfile
  */
 #if !defined( HAVE_LOCAL_LIBCFILE )
@@ -32,7 +45,7 @@
 
 #else
 #define LIBCFILE_EXTERN		/* extern */
-#define LIBCFILE_EXTERN_VARIABLE	extern
+#define LIBCFILE_EXTERN_VARIABLE	LIBCFILE_INTERNAL
 
 #endif /* !defined( HAVE_LOCAL_LIBCFILE ) */
 
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20201121
+# Version: 20260602
 
-if test -f ${PWD}/libcfile/.libs/libcfile.1.dylib && test -f ./pycfile/.libs/pycfile.so;
+if test -f ${PWD}/libcfile/.libs/libcfile.1.dylib && test -f ./pycfile/.libs/pycfile.so
 then
-	install_name_tool -change /usr/local/lib/libcfile.1.dylib ${PWD}/libcfile/.libs/libcfile.1.dylib ./pycfile/.libs/pycfile.so;
+	install_name_tool -change /usr/local/lib/libcfile.1.dylib ${PWD}/libcfile/.libs/libcfile.1.dylib ./pycfile/.libs/pycfile.so
 fi
 
-make check CHECK_WITH_STDERR=1;
-RESULT=$?;
+make check $@
+RESULT=$?
 
-if test ${RESULT} -ne 0 && test -f tests/test-suite.log;
+if test ${RESULT} -ne 0 && test -f tests/test-suite.log
 then
-	cat tests/test-suite.log;
+	cat tests/test-suite.log
 fi
-exit ${RESULT};
+exit ${RESULT}
 
