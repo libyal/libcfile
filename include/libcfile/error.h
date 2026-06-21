4dd1d87906455e36028fd351434318dd7af26ad3
Joachim Metz <joachim.metz@gmail.com>
2026-06-21 18:19:29 +0200

    Worked on tests and changes for CI tests

--- include/libcfile/error.h
+++ include/libcfile/error.h
@@ -82,7 +82,7 @@ enum LIBCFILE_ARGUMENT_ERROR
 	 */
 	LIBCFILE_ARGUMENT_ERROR_UNSUPPORTED_VALUE	= 8,
 
-	/* The argument contains a value that conficts with another argument
+	/* The argument contains a value that conflicts with another argument
 	 */
 	LIBCFILE_ARGUMENT_ERROR_CONFLICTING_VALUE	= 9
 };
@@ -214,7 +214,7 @@ enum LIBCFILE_OUTPUT_ERROR
 {
 	LIBCFILE_OUTPUT_ERROR_GENERIC			= 0,
 
-	/* There is insuficient space to write the output
+	/* There is insufficient space to write the output
 	 */
 	LIBCFILE_OUTPUT_ERROR_INSUFFICIENT_SPACE	= 1
 };
